2f89792e9ea9ad5ce5628b8cc4902c52fbeb78a8
wangyakun <wangyakun@dyrs.com.cn>
2018-05-30 12:07:37 +0800

    u

---
+++ tmp/smarty_templates_c/32bb9afe319df1c4360179b5e78cddffd7839e27_0.file.index.html.php
@@ -0,0 +1,36 @@
+<?php
+/* Smarty version 3.1.30, created on 2018-05-30 11:36:12
+  from "E:\code\zendstudio_wokplace\9.4-ep_v2\app\http\view\default\index\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5b0e1c2c871519_34163521',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '32bb9afe319df1c4360179b5e78cddffd7839e27' => 
+    array (
+      0 => 'E:\\code\\zendstudio_wokplace\\9.4-ep_v2\\app\\http\\view\\default\\index\\index.html',
+      1 => 1527651371,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b0e1c2c871519_34163521 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+
+</body>
+</html><?php }
+}
