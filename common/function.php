51a7c5f252fcf4b2636fe367330c227eafa12fd8
wangyakun <wangyakun@dyrs.com.cn>
2018-09-04 11:43:13 +0800

    添加堆栈调试函数

--- common/function.php
+++ common/function.php
@@ -2,10 +2,23 @@
 function pr(){
     $args = func_get_args();
     echo '<pre>';
-    foreach ($args as $v){
-        print_r($v);
+    foreach( $args as $v ){
+        print_r( $v );
         echo '<hr>';
     }
     echo '<pre/>';
-    exit;
+    exit();
+}
+/**
+ * 打印函数调用堆栈，方便调试
+ * @return string
+ */
+function prstack(){
+    $array = debug_backtrace();
+    // print_r($array);//信息很齐全
+    unset( $array[0] );
+    foreach( $array as $row ){
+        $html .= $row['file'] . ':' . $row['line'] . '行,调用方法:' . $row['function'] . "<p>";
+    }
+    return $html;
 }
